2716c99edee3c7261921668ddcfbb34160c644a3
THUAANFY <lethuanphiqb@gamil.com>
2025-03-15 17:39:10 +0700

    THUANFY: Fifth Commit

--- asm/src/main/java/poly/asm/Controller/HomeController.java
+++ asm/src/main/java/poly/asm/Controller/HomeController.java
@@ -1,9 +1,15 @@
 package poly.asm.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 
 @Controller
 public class HomeController {
-
+    @GetMapping("/index")
+    public String index(Model model) {
+        return "Home/index";
+    }
+    
 }

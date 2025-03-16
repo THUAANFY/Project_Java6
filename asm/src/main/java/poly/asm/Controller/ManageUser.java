6c0dbb769fc437dada44806fb80be2ddc85e0e98
DN03-hi <dungnhi11122003@gmail.com>
2025-03-16 15:59:42 +0700

    Dung Nhi commit lan 2

---
+++ asm/src/main/java/poly/asm/Controller/ManageUser.java
@@ -0,0 +1,15 @@
+package poly.asm.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ManageUser {
+
+    @GetMapping("/manageUser")
+    public String manageUser(Model model){
+        return "Admin/ManageUser";
+    }
+
+}
